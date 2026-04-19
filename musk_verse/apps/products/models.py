d954927563399c8031d2851b07c142977cda8948
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-04-19 15:19:27 +0100

    Update models.py
    Save

--- musk_verse/apps/products/models.py
+++ musk_verse/apps/products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    image = models.ImageField(upload_to='cars/', blank=True, null=True)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class FanCard(models.Model):
+    name = models.CharField(max_length=100)
+    benefits = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='fancards/', blank=True, null=True)
+
+class MembershipCard(models.Model):
+    TIER_CHOICES = [('bronze', 'Bronze'), ('silver', 'Silver'), ('gold', 'Gold'), ('platinum', 'Platinum')]
+    name = models.CharField(max_length=100)
+    tier = models.CharField(max_length=20, choices=TIER_CHOICES)
+    benefits = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Stock(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10, unique=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    available_quantity = models.PositiveIntegerField()
+
+class CryptoAsset(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10, unique=True)
+    price = models.DecimalField(max_digits=16, decimal_places=8)
+    fractional_allowed = models.BooleanField(default=True)
+
+class InvestmentPlan(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    roi_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    duration = models.PositiveIntegerField(help_text="Duration in days")
+    minimum_amount = models.DecimalField(max_digits=12, decimal_places=2)
