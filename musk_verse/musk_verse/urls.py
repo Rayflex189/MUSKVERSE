cf15e6daf903eeb2b6b70d8427fb45f013876d5d
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-04-19 16:39:36 +0100

    Update urls.py
    Save

--- musk_verse/musk_verse/urls.py
+++ musk_verse/musk_verse/urls.py
@@ -1,23 +1,24 @@
-"""
-URL configuration for musk_verse project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+admin.site.site_title = settings.ADMIN_SITE_TITLE
+admin.site.index_title = settings.ADMIN_INDEX_TITLE
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include('apps.api.urls')),
+    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
+    path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
+    path('verify-otp/', TemplateView.as_view(template_name='verify_otp.html'), name='verify_otp'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('profile-setup/', TemplateView.as_view(template_name='profile_setup.html'), name='profile_setup'),
+    path('marketplace/', TemplateView.as_view(template_name='marketplace.html'), name='marketplace'),
+    path('transactions/', TemplateView.as_view(template_name='transaction_history.html'), name='transactions'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
