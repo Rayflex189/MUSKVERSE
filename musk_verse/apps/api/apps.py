087e97ed6729d88e3e78a0c883c3831965eca638
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-04-21 15:55:44 +0000

    save

--- musk_verse/apps/api/apps.py
+++ musk_verse/apps/api/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
-class ApiConfig(AppConfig):
-    name = "api"
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.api'
